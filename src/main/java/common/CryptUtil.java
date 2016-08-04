86d1f5748c7a1c97a00ba9c9e3d74effb43e0624
vietpq4 <quocvietwork@gmail.com>
2016-08-04 17:02:48 +0700

    update

--- src/main/java/common/CryptUtil.java
+++ src/main/java/common/CryptUtil.java
@@ -3,11 +3,27 @@ package common;
 import java.io.UnsupportedEncodingException;
 
 public class CryptUtil {
-	public static String encrypt(String input, String sessionId) throws UnsupportedEncodingException{
-		return null;
-	}
-	
-	public static String decrypt(String input, String sessionId) throws UnsupportedEncodingException{
-		return null;
-	}
+    public static String encrypt(String input, String sessionId) throws UnsupportedEncodingException {
+        int key = 0;
+        int limit = 15;
+        while (key == 0) {
+            key = sessionId.charAt(0) % limit++;
+        }
+        StringBuffer output = new StringBuffer("");
+
+        for (char c : input.toCharArray()) {
+            if ('A' <= c && c <= 'Z') {
+                output.append((char) ((c + key) > 90 ? (c + key - 90 + 65) : c + key));
+            } else if ('a' <= c && c <= 'z') {
+                output.append((char) ((c + key) > 122 ? (c + key - 122 + 97) : c + key));
+            } else {
+                throw new UnsupportedEncodingException();
+            }
+        }
+        return output.toString();
+    }
+
+    public static String decrypt(String input, String sessionId) throws UnsupportedEncodingException {
+        return null;
+    }
 }
